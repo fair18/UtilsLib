57c16be61cdb93a97c9c03d1a7834d3df87aa3cb
Iurii Borysiuk <iborysiuk@ap1.io>
2016-09-13 13:37:33 -0400

    Initial commit

---
+++ utils-lib/src/main/java/com/iborysiuk/lib/utils/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.iborysiuk.lib.utils;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yuriy Borysiuk on 9/13/2016.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+}
